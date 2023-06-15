4a2e054645d5d20ee925a93b171e02a6e192ac99
William Ballenthin <William.Ballenthin+MNDT@mandiant.com>
2023-06-16 00:13:17 +0200

    initial web renderer

--- wb_graph.py
+++ wb_graph.py
@@ -7,11 +7,14 @@ from typing import List
 from dataclasses import dataclass
 
 import ida_gdl
+import ida_name
 import ida_bytes
 import ida_lines
 import ida_funcs
 import ida_graph
+import ida_idaapi
 import ida_kernwin
+import idautils
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -24,6 +27,7 @@ logger = logging.getLogger(__name__)
 # updates:
 #   - use dataclasses instead of custom classes.
 #   - resolve color name during lexing, not during rendering.
+#   - parse addresses into their own symbols
 
 # inverse mapping of color value to name.
 # ref: https://www.hex-rays.com/products/ida/support/sdkdoc/group___s_c_o_l_o_r__.html#ga6052470f86411b8b5ffdf4af4bbee225
@@ -81,9 +85,67 @@ INV_COLORS = {
     0x2C: 'COLOR_OPND4',    #= COLOR_ADDR+4, // Instruction operand 4
     0x2D: 'COLOR_OPND5',    #= COLOR_ADDR+5, // Instruction operand 5
     0x2E: 'COLOR_OPND6',    #= COLOR_ADDR+6, // Instruction operand 6
+    0x2F: 'COLOR_OPND7',    #= COLOR_ADDR+7, // Instruction operand 7
+    0x30: 'COLOR_OPND8',    #= COLOR_ADDR+8, // Instruction operand 8
 
     0x32: 'COLOR_UTF8',     #= COLOR_ADDR+10;// Following text is UTF-8 encoded
+    0x33: 'RESERVED1',      #= COLOR_ADDR+11;// This tag is reserved for internal IDA use.
+    0x34: 'LUMINA',         #= COLOR_ADDR+12;// Lumina-related, only for the navigation band.
 }
+
+# 000000	 //
+# f2f8f8	 //Default color
+# a47262	 //Regular comment
+# a47262	 //Repeatable comment
+# a47262	 //Automatic comment
+# 6cb8ff	 //Instruction
+# fde98b	 //Dummy Data Name
+# fde98b	 //Regular Data Name
+# fde98b	 //Demangled Name
+# fde98b	 //Punctuation
+# 7bfa50	 //Char constant in instruction
+# 7bfa50	 //String constant in instruction
+# 7bfa50	 //Numeric constant in instruction
+# 6cb8ff	 //Void operand
+# 7bfa50	 //Code reference
+# f993bd	 //Data reference
+# 5555ff	 //Code reference to tail byte
+# 8cfaf1	 //Data reference to tail byte
+# 5555ff	 //Error or problem
+# c0c0c0	 //Line prefix
+# fde98b	 //Binary line prefix bytes
+# a47262	 //Extra line
+# fde98b	 //Alternative operand
+# a47262	 //Hidden name
+# ff8080	 //Library function name
+# 7bfa50	 //Local variable name
+# fde98b	 //Dummy code name
+# fde98b	 //Assembler directive
+# f993bd	 //Macro
+# 7bfa50	 //String constant in data directive
+# 7bfa50	 //Char constant in data directive
+# 7bfa50	 //Numeric constant in data directive
+# fde98b	 //Keywords
+# fde98b	 //Register name
+# c679ff	 //Imported name
+# 8cfaf1	 //Segment name
+# fde98b	 //Dummy unknown name
+# fde98b	 //Regular code name
+# fde98b	 //Regular unknown name
+# a47262	 //Collapsed line
+# 000000	 //Max color number
+# 362a28	 //Line prefix: library function
+# afbbc0	 //Line prefix: regular function
+# fde98b	 //Line prefix: instruction
+# a47262	 //Line prefix: data
+# 5555ff	 //Line prefix: unexplored
+# a47262	 //Line prefix: externs
+# 8cfaf1	 //Line prefix: current item
+# c679ff	 //Line prefix: current line
+# 5a4744	 //Punctuation
+# 5a4744	 //Opcode bytes
+# 000000	 //Manual operand
+# 7bfa50	 //Error
 
 
 @dataclass
@@ -93,6 +155,15 @@ class StringSymbol:
 
     def __str__(self):
         return 'STRING=' + self.string
+
+
+@dataclass
+class AddressSymbol:
+    address: int
+    type: str = 'address'
+
+    def __str__(self):
+        return 'ADDRESS=' + hex(self.address)
 
 
 @dataclass
@@ -132,7 +203,6 @@ def lex(curline):
     Returns:
       generator: generator of Symbol subclass instances
     '''
-
     offset = 0
     cur_word = []
     while offset < len(curline):
@@ -147,8 +217,18 @@ def lex(curline):
             offset += 1
             color = curline[offset]
 
-            yield ColorOnSymbol(INV_COLORS[ord(color)])
             offset += 1
+
+            if INV_COLORS[ord(color)] == "COLOR_ADDR":
+                # assume we're in ida64!
+                # next 16 characters are hex-encoded number
+                # like: 0000000000470670
+                size = 0x10
+                address = int(curline[offset:offset+size], 0x10)
+                offset += size
+                yield AddressSymbol(address)
+            else:
+                yield ColorOnSymbol(INV_COLORS[ord(color)])
 
         elif c == ida_lines.COLOR_OFF:
             if cur_word:
@@ -248,11 +328,18 @@ class DisassemblyLine:
 
 
 @dataclass
+class Location:
+    address: int
+    lines: List[DisassemblyLine]
+    name: str | None = None
+
+
+@dataclass
 class BasicBlock:
     rect: Rectangle
     range: Range
     bytes: str  # hex-encoded bytes
-    disassembly: List[DisassemblyLine]
+    locations: List[Location]
 
 
 @dataclass
@@ -273,6 +360,33 @@ class Graph:
     edges: List[Edge]
 
 
+def export_locations(start, end):
+    locations = []
+
+    for head in idautils.Heads(start, end):
+        disassembly: ida_kernwin.disasm_text_t = ida_kernwin.disasm_text_t()
+        ida_kernwin.gen_disasm_text(disassembly, head, head + 1, False)
+
+        lines = [
+            DisassemblyLine(
+                tokens=list(lex(line.line)),
+                bg_color=line.bg_color,
+                prefix_color=line.prefix_color,
+                is_default=line.is_default,
+            ) for line in disassembly
+        ]
+
+        name = ida_name.get_name(head)
+
+        locations.append(Location(
+            address=head,
+            lines=lines,
+            name=name,
+        ))
+
+    return locations
+
+
 def export_current_graph():
     va = ida_kernwin.get_screen_ea()
     f = ida_funcs.get_func(va)
@@ -287,6 +401,11 @@ def export_current_graph():
 
     gv: ida_kernwin.graph_viewer_t = ida_kernwin.get_current_viewer()
     g: ida_graph.mutable_graph_t = ida_graph.get_viewer_graph(gv)
+
+    # while its possible to use ida_graph.create_disasm_graph(va)
+    # for arbitrary va
+    # it doesn't have an associated rendering context
+    # so the resulting offsets/points/rects don't make sense.
 
     graph: Graph = Graph(basic_blocks=[], edges=[])
 
@@ -313,14 +432,7 @@ def export_current_graph():
                 basic_block.start_ea,
                 basic_block.end_ea - basic_block.start_ea)
             ).decode('ascii'),
-            disassembly=[
-                DisassemblyLine(
-                    tokens=list(lex(line.line)),
-                    bg_color=line.bg_color,
-                    prefix_color=line.prefix_color,
-                    is_default=line.is_default,
-                ) for line in disassembly
-            ]
+            locations=export_locations(basic_block.start_ea, basic_block.end_ea),
         ))
 
         for succ in basic_block.succs():
@@ -362,7 +474,7 @@ def main():
 
     path = ida_kernwin.ask_file(True, "*", "json file to save graph")
     if not path:
-        print(doc)
+        #print(doc)
         return
 
     pathlib.Path(path).write_text(doc, encoding="utf-8")
