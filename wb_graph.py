88066ddbbf37a54135af39467f29238aad1df402
Willi Ballenthin <willi.ballenthin@gmail.com>
2023-06-20 09:32:38 +0200

    better export

--- wb_graph.py
+++ wb_graph.py
@@ -12,12 +12,27 @@ import ida_bytes
 import ida_lines
 import ida_funcs
 import ida_graph
-import ida_idaapi
 import ida_kernwin
 import idautils
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
+
+
+class Address(str):
+    # stored as string, so that we can encode to javascript
+
+    @classmethod
+    def from_int(cls, value):
+        return cls(f"{value:016x}")
+
+
+class Color(str):
+    # stored as string, so that we can encode to javascript
+
+    @classmethod
+    def from_int(cls, value):
+        return cls(hex(value))
 
 
 ###############################################################################
@@ -31,7 +46,8 @@ logger = logging.getLogger(__name__)
 
 # inverse mapping of color value to name.
 # ref: https://www.hex-rays.com/products/ida/support/sdkdoc/group___s_c_o_l_o_r__.html#ga6052470f86411b8b5ffdf4af4bbee225
-INV_COLORS = {
+COLOR_NAMES_BY_INDEX = {
+    0x0: 'COLOR_UNSET',    #= 0x00,         // willi made this value up.
     0x1: 'COLOR_DEFAULT',  #= 0x01,         // Default
     0x2: 'COLOR_REGCMT',   #= 0x02,         // Regular comment
     0x3: 'COLOR_RPTCMT',   #= 0x03,         // Repeatable comment (comment defined somewhere else)
@@ -93,60 +109,6 @@ INV_COLORS = {
     0x34: 'LUMINA',         #= COLOR_ADDR+12;// Lumina-related, only for the navigation band.
 }
 
-# 000000	 //
-# f2f8f8	 //Default color
-# a47262	 //Regular comment
-# a47262	 //Repeatable comment
-# a47262	 //Automatic comment
-# 6cb8ff	 //Instruction
-# fde98b	 //Dummy Data Name
-# fde98b	 //Regular Data Name
-# fde98b	 //Demangled Name
-# fde98b	 //Punctuation
-# 7bfa50	 //Char constant in instruction
-# 7bfa50	 //String constant in instruction
-# 7bfa50	 //Numeric constant in instruction
-# 6cb8ff	 //Void operand
-# 7bfa50	 //Code reference
-# f993bd	 //Data reference
-# 5555ff	 //Code reference to tail byte
-# 8cfaf1	 //Data reference to tail byte
-# 5555ff	 //Error or problem
-# c0c0c0	 //Line prefix
-# fde98b	 //Binary line prefix bytes
-# a47262	 //Extra line
-# fde98b	 //Alternative operand
-# a47262	 //Hidden name
-# ff8080	 //Library function name
-# 7bfa50	 //Local variable name
-# fde98b	 //Dummy code name
-# fde98b	 //Assembler directive
-# f993bd	 //Macro
-# 7bfa50	 //String constant in data directive
-# 7bfa50	 //Char constant in data directive
-# 7bfa50	 //Numeric constant in data directive
-# fde98b	 //Keywords
-# fde98b	 //Register name
-# c679ff	 //Imported name
-# 8cfaf1	 //Segment name
-# fde98b	 //Dummy unknown name
-# fde98b	 //Regular code name
-# fde98b	 //Regular unknown name
-# a47262	 //Collapsed line
-# 000000	 //Max color number
-# 362a28	 //Line prefix: library function
-# afbbc0	 //Line prefix: regular function
-# fde98b	 //Line prefix: instruction
-# a47262	 //Line prefix: data
-# 5555ff	 //Line prefix: unexplored
-# a47262	 //Line prefix: externs
-# 8cfaf1	 //Line prefix: current item
-# c679ff	 //Line prefix: current line
-# 5a4744	 //Punctuation
-# 5a4744	 //Opcode bytes
-# 000000	 //Manual operand
-# 7bfa50	 //Error
-
 
 @dataclass
 class StringSymbol:
@@ -159,7 +121,7 @@ class StringSymbol:
 
 @dataclass
 class AddressSymbol:
-    address: int
+    address: Address
     type: str = 'address'
 
     def __str__(self):
@@ -219,16 +181,16 @@ def lex(curline):
 
             offset += 1
 
-            if INV_COLORS[ord(color)] == "COLOR_ADDR":
+            if COLOR_NAMES_BY_INDEX[ord(color)] == "COLOR_ADDR":
                 # assume we're in ida64!
                 # next 16 characters are hex-encoded number
                 # like: 0000000000470670
                 size = 0x10
                 address = int(curline[offset:offset+size], 0x10)
                 offset += size
-                yield AddressSymbol(address)
+                yield AddressSymbol(Address.from_int(address))
             else:
-                yield ColorOnSymbol(INV_COLORS[ord(color)])
+                yield ColorOnSymbol(COLOR_NAMES_BY_INDEX[ord(color)])
 
         elif c == ida_lines.COLOR_OFF:
             if cur_word:
@@ -238,7 +200,7 @@ def lex(curline):
             offset += 1
             color = curline[offset]
 
-            yield ColorOffSymbol(INV_COLORS[ord(color)])
+            yield ColorOffSymbol(COLOR_NAMES_BY_INDEX[ord(color)])
             offset += 1
 
         elif c == ida_lines.COLOR_ESC:
@@ -285,7 +247,6 @@ def get_color_at_char(curline, index):
 
 def get_token_at_char(curline, index):
     curlen = 0
-    curcolor = 0
     for sym in lex(curline):
         if sym.type == 'string':
             curlen += len(sym.string)
@@ -300,7 +261,6 @@ def get_token_at_char(curline, index):
 #### end: idapython_lex_curline
 ###############################################################################
 
-
 @dataclass
 class Rectangle:
     left: int
@@ -311,25 +271,22 @@ class Rectangle:
 
 @dataclass
 class Range:
-    start: int
-    length: int
-
-    @property
-    def end(self):
-        return self.start + self.length
+    start: Address
+    end: Address
 
 
 @dataclass
 class DisassemblyLine:
     tokens: List[str]
-    bg_color: int
-    prefix_color: int
+    # often broken
+    bg_color: Color
+    prefix_color_name: str  # one of the COLOR_* constants
     is_default: bool
 
 
 @dataclass
 class Location:
-    address: int
+    address: Address
     lines: List[DisassemblyLine]
     name: str | None = None
 
@@ -340,6 +297,8 @@ class BasicBlock:
     range: Range
     bytes: str  # hex-encoded bytes
     locations: List[Location]
+    bg_color: Color
+    frame_color: Color
 
 
 @dataclass
@@ -350,7 +309,8 @@ class Point:
 
 @dataclass
 class Edge:
-    color: int
+    # often broken
+    color: Color
     points: List[Point]
 
 
@@ -370,8 +330,10 @@ def export_locations(start, end):
         lines = [
             DisassemblyLine(
                 tokens=list(lex(line.line)),
-                bg_color=line.bg_color,
-                prefix_color=line.prefix_color,
+                # these colors often don't make sense (bgcolor_t)
+                bg_color=Color.from_int(line.bg_color),
+                # these colors look ok
+                prefix_color_name=COLOR_NAMES_BY_INDEX.get(line.prefix_color, "COLOR_UNSET"),
                 is_default=line.is_default,
             ) for line in disassembly
         ]
@@ -379,7 +341,7 @@ def export_locations(start, end):
         name = ida_name.get_name(head)
 
         locations.append(Location(
-            address=head,
+            address=Address.from_int(head),
             lines=lines,
             name=name,
         ))
@@ -403,9 +365,11 @@ def export_current_graph():
     g: ida_graph.mutable_graph_t = ida_graph.get_viewer_graph(gv)
 
     # while its possible to use ida_graph.create_disasm_graph(va)
-    # for arbitrary va
-    # it doesn't have an associated rendering context
+    # for arbitrary va, it doesn't have an associated rendering context
     # so the resulting offsets/points/rects don't make sense.
+    #
+    # therefore, we do have to have a UI available,
+    # and manipulate the current view in order to export graphs.
 
     graph: Graph = Graph(basic_blocks=[], edges=[])
 
@@ -413,9 +377,8 @@ def export_current_graph():
         # is the order guaranteed to be the same here?
         rect: ida_graph.rect_t = g.nodes[i]
         basic_block: ida_gdl.BasicBlock = flowchart[i]
-
-        disassembly: ida_kernwin.disasm_text_t = ida_kernwin.disasm_text_t()
-        ida_kernwin.gen_disasm_text(disassembly, basic_block.start_ea, basic_block.end_ea, False)
+        info = ida_graph.node_info_t()
+        ida_graph.viewer_get_node_info(gv, info, i)
 
         graph.basic_blocks.append(BasicBlock(
             rect=Rectangle(
@@ -425,14 +388,18 @@ def export_current_graph():
                 bottom=rect.bottom,
             ),
             range=Range(
-                start=basic_block.start_ea,
-                length=basic_block.end_ea - basic_block.start_ea,
+                start=Address.from_int(basic_block.start_ea),
+                end=Address.from_int(basic_block.end_ea),
             ),
             bytes=binascii.hexlify(ida_bytes.get_bytes(
                 basic_block.start_ea,
                 basic_block.end_ea - basic_block.start_ea)
             ).decode('ascii'),
             locations=export_locations(basic_block.start_ea, basic_block.end_ea),
+            # seems to usually be 0xFFFFFFFF, which is unset.
+            bg_color=Color.from_int(info.bg_color),
+            # seems to usually be 0xFFFFFFFF, which is unset.
+            frame_color=Color.from_int(info.frame_color),
         ))
 
         for succ in basic_block.succs():
@@ -453,7 +420,19 @@ def export_current_graph():
             points.append(Point(dst_node.left + edge.dstoff, dst_node.top))
 
             graph.edges.append(Edge(
-                color=edge.color,
+                # this color seems to be wrong?
+                # seeing values like:
+                #  - 0x8113e243
+                #  - 0x8113e245
+                #  - 0x8113e244
+                # which are oddly consecutive and don't appear to be RGB-like colors.
+                #
+                # C++ SDK seems to give more reasonable values:
+                # https://github.com/williballenthin/ida-graph-exporter/blob/7c5bd19ebb6620e91af5656f1cca739b0fb9fb21/example/id.json#L339
+                #
+                # i debugged IDA and confirmed these values are what's stored in the edge_info_t struct in memory,
+                # its not a python binding issue.
+                color=Color.from_int(edge.color),
                 points=points,
             ))
 
@@ -464,20 +443,25 @@ class DataclassJSONEncoder(json.JSONEncoder):
     def default(self, o):
         if dataclasses.is_dataclass(o):
             return dataclasses.asdict(o)
-        return super().default(o)
+        else:
+            return super().default(o)
 
 
-def main():
+def do_export_current_graph():
     graph = export_current_graph()
 
     doc = json.dumps(graph, cls=DataclassJSONEncoder, indent=2, sort_keys=True)
 
     path = ida_kernwin.ask_file(True, "*", "json file to save graph")
     if not path:
-        #print(doc)
+        print(doc)
         return
 
     pathlib.Path(path).write_text(doc, encoding="utf-8")
+
+
+def main():
+    do_export_current_graph()
 
 
 if __name__ == "__main__":
